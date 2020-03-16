d6d265532731d5b163754be61c848b43e96df4a6
Ihor <55961576+IhorHolubovskiy@users.noreply.github.com>
2020-03-16 21:40:34 +0200

    Update game.js

--- game.js
+++ game.js
@@ -8,18 +8,18 @@ var fg = new Image(); // Создание объекта
 var pipeUp = new Image(); // Создание объекта
 var pipeBottom = new Image(); // Создание объекта
 
-bird.src = "../game/img/bird.png"; // Указание нужного изображения
-bg.src = "../game/img/bg.png"; // Аналогично
-fg.src = "../game/img/fg.png"; // Аналогично
-pipeUp.src = "../game/img/pipeUp.png"; // Аналогично
-pipeBottom.src = "../game/img/pipeBottom.png"; // Аналогично
+bird.src = "img/bird.png"; // Указание нужного изображения
+bg.src = "img/bg.png"; // Аналогично
+fg.src = "img/fg.png"; // Аналогично
+pipeUp.src = "img/pipeUp.png"; // Аналогично
+pipeBottom.src = "img/pipeBottom.png"; // Аналогично
 
 
 var fly = new Audio();
 var score_audio = new Audio();
 
-fly.src = "../game/petia2.aac";
-score_audio.src = "../game/petia1.aac";
+fly.src = "petia2.aac";
+score_audio.src = "petia1.aac";
 var gap = 90;
 
 document.addEventListener("keydown", moveUp);
